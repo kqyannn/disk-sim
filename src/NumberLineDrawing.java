db5ad684670213cd299a8ec6b057d9b76e4a80fd
FrancisEspina <faespina@up.edu.ph>
2023-05-29 03:10:20 +0800

    output that shit

--- src/NumberLineDrawing.java
+++ src/NumberLineDrawing.java
@@ -5,12 +5,17 @@ import java.util.*;
 public class NumberLineDrawing extends JPanel {
     private int start;
     private int end;
+    public int[] results;
+    public int head;
     private ArrayList<Integer> sequence;
 
-    public NumberLineDrawing(int start, int end, ArrayList<Integer> sequence) {
+    public NumberLineDrawing(int start, int end, ArrayList<Integer> sequence, int[] results, int head) {
         this.start = start;
         this.end = end;
         this.sequence = sequence;
+        this.results = results;
+        this.head = head;
+
     }
 
     @Override
@@ -20,7 +25,7 @@ public class NumberLineDrawing extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         int width = getWidth();
-        int height = getHeight();
+        int height = 100;
 
         // Set up drawing properties
         g2d.setColor(Color.BLACK);
@@ -43,11 +48,13 @@ public class NumberLineDrawing extends JPanel {
         g2d.setStroke(new BasicStroke(3));
         g2d.drawLine(0, height / 2, width, height / 2);
 
-        g2d.drawLine(0, 0, 500, 100);
-        g2d.fillOval(495, 95, 10, 10);
-        g2d.drawLine(500, 100, 200, 300);
-        g2d.fillOval(200-5, 300-5, 10, 10);
-        g2d.drawLine(200, 300, 90, 60);
+       
+
+        // g2d.drawLine(0, 0, 500, 100);
+        // g2d.fillOval(495, 95, 10, 10);
+        // g2d.drawLine(500, 100, 200, 300);
+        // g2d.fillOval(200-5, 300-5, 10, 10);
+        // g2d.drawLine(200, 300, 90, 60);
 
         // Plot the sequence on the number line
         int sequenceSize = sequence.size();
@@ -57,16 +64,72 @@ public class NumberLineDrawing extends JPanel {
             int number = sequence.get(i);
 
             int x = (number - start) * tickInterval;
+            System.out.println(start);
             x = x + 10;
             g2d.drawLine(x, height / 2 - 10, x, height / 2 + 10); // ticks
             // Adjust y position to avoid overlapping with tick marks
             int y = height / 2 - 10 - sequenceMarkerSize;
+           
 
             g2d.setColor(Color.RED);
             g2d.fillOval(x - sequenceMarkerSize / 2, y, sequenceMarkerSize, sequenceMarkerSize);
             g2d.setColor(Color.BLACK);
             g2d.drawString(String.valueOf(number), x - 5, y - 5);
+            
         }
+        lineWriter(g2d, head);
+
+        
+       
+    }
+    public void lineWriter(Graphics g2d, int head) {
+
+        int length = Math.abs(start) + Math.abs(end) + 1;
+        int width = getWidth();
+        int ticks = width / length;
+        int number = head;
+        int startY = 50;
+        int newX = 0;
+        int nextY = 0;
+        int startX = (number - 0) * ticks;
+        startX = startX + 10;
+        // startY = 50;
+       
+        for(int i = 0; i < results.length; i++){
+            if(i == 0){
+                number = head;
+            }
+            else{
+                number = results[i];
+            }
+            
+    
+
+            int nextX = (results[i] - 0) * ticks;
+            nextY = nextY + 50;
+            nextX = nextX + 10;
+            
+            
+
+               
+                       
+                g2d.drawLine(startX, startY, nextX, nextY);
+                g2d.fillOval(startX-6, startY-6, 12, 12);
+                g2d.drawString(String.valueOf(number), nextX, nextY + 20);
+                startX = nextX;
+                startY = nextY;
+                
+             
+                // y = y + 50;
+                
+           
+            
+
+
+        }
+        g2d.fillOval(startX-6, startY-6, 12, 12);
+        
+        
     }
 
     public static void main(String[] args) {
@@ -79,20 +142,26 @@ public class NumberLineDrawing extends JPanel {
         int end = 199;
 
         ArrayList<Integer> sequence = new ArrayList<>();
-        int[] array = {20, 30, 15, 90, 60, 50, 10, 15, 199};
+        int[] array = {53,98,183,37,122,14,124,65,67};
+        int head = 53;
+        int[] results = {53,98,183,37,122,14,124,65,67};
         for(int i = 0; i < array.length; i++){
             sequence.add(array[i]);
         }
+        
+
+    
+        
         
         
 
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame("Number Line Drawing");
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setSize(800, 200);
+            frame.setSize(1080, 720);
             frame.setLocationRelativeTo(null);
 
-            NumberLineDrawing numberLineDrawing = new NumberLineDrawing(start, end, sequence);
+            NumberLineDrawing numberLineDrawing = new NumberLineDrawing(start, end, sequence, results, head);
             frame.add(numberLineDrawing);
 
             frame.setVisible(true);
